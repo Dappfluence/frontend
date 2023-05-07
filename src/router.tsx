b0770ff5350a765ef6128878933c73f023a28f11
Hanover <gerrardthebanana@gmail.com>
2023-05-07 15:25:32 +0200

    feat: add flowbite

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import {createBrowserRouter} from "react-router-dom";
 import Home from "./pages/Home";
+import Collaborations from "./pages/Collaborations";
 
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Home/>
-	}
+	},
+  {path: "/collabs", element: <Collaborations />}
 ])
 
 export default router
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",], theme: {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'], theme: {
     extend: {
       colors: {
         "orange": {'from': '#CE4B43', 'to': '#F1BE44'}, "blackcard": "#14181E"
@@ -10,6 +10,6 @@ export default {
         fake: 'url(@/assets/background.png)',
       }
     },
-  }, plugins: [],
+  }, plugins: [import('flowbite/plugin')],
 }
 
