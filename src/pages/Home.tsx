a25cd09fb840bfe8b88077a3bba5988e1ab8767a
jakhongir.tashpulatov <jakhongir.tashpulatov@publicisgroupe.cz>
2023-05-07 14:36:10 +0200

    cleared from trash

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,6 @@
 import React from "react";
-import {Game} from "../types";
-import GameTile from "../components/tiles/GameTile";
-import {useGamesQuery} from "../api/games";
-
-
 const Home: React.FC = () => {
-
-	const {data: games = []} = useGamesQuery();
-
-
-	const renderGames = () => {
-		return games.map((e: Game) => <div className={'col-span-1'}>
-			<GameTile {...e} key={e.name} />
-		</div>)
-	}
-
 	return <div className={'border border-sky-600 grid grid-cols-3'}>
-		{renderGames()}
 	</div>
 }
 
