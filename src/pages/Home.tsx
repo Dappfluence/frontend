662d21cb4b2d18e36b0389eeb8ccdfb6a4013d5c
jakhongir.tashpulatov <jakhongir.tashpulatov@publicisgroupe.cz>
2023-05-09 20:37:34 +0200

    fixed header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,16 @@ import backgroundImage from "../assets/background.png";
 import grid from "../assets/grid.png";
 
 const Home: React.FC = () => {
-  return <div className={' w-full min-h-[100vh]'}>
-    <div className={'absolute top-0 left-0 w-full h-[80vh] z-[0] rounded-[50px] rounded-t-none overflow-hidden bg-gradient-to-r from-purple-dark to-purple-light'}>
-      <div className={'w-full h-full absolute bg-cover z-[1]'} style={{backgroundImage: `url(${backgroundImage})`}}></div>
+  return <div className={' w-full min-h-[100vh] pt-[96px]'}>
+    <div
+      className={'absolute top-0 left-0 w-full h-[80vh] z-0 rounded-[50px] rounded-t-none overflow-hidden bg-gradient-to-r from-purple-dark to-purple-light'}>
+      <div className={'w-full h-full absolute bg-cover z-[1]'}
+           style={{backgroundImage: `url(${backgroundImage})`}}></div>
       <div className={'w-full h-full absolute bg-cover opacity-50 '} style={{backgroundImage: `url(${grid})`}}></div>
     </div>
-    hey
+    <div className={' absolute border border-red-500 w-full  my-48 z-10'}>
+      hey
+    </div>
   </div>
 }
 
--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -6,7 +6,7 @@ import logo from "../assets/logo.png";
 const Header: React.FC = () => {
 
   return <div
-    className={' absolute z-[10] top-0  text-white w-full max-h-64 items-center flex flex-row justify-between py-6 px-20 bg-black rounded-[50px] rounded-t-none'}>
+    className={' fixed z-[10] top-0  text-white w-full h-[96px] items-center flex flex-row justify-between py-6 px-20 bg-black rounded-[50px] rounded-t-none'}>
     <div className={'flex flex-row gap-3 justify-start items-center'}>
       <img src={logo} alt=""/>
       <h1 className={'text-2xl'}>CollabY</h1>
