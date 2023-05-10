0278f3341f4c409266304db774f41f07aa24eb3d
Hanover <gerrardthebanana@gmail.com>
2023-05-10 17:35:38 +0200

    feat: add basic collaboration page

--- src/pages/Collaborations.tsx
+++ src/pages/Collaborations.tsx
@@ -4,12 +4,12 @@ import CollaborationCard from "../ui/collaborations/components/CollaborationCard
 
 const Collaborations: FC = () => {
   return (
-    <div className={'mt-12 container mx-auto'}>
-      <h1>
+    <div className={'mt-[144px] container mx-auto'}>
+      <h1 className={'text-5xl font-bold'}>
         1 504 Collaborations
       </h1>
 
-      <div className={'flex gap-5'}>
+      <div className={'flex gap-5 mt-6'}>
         <div className={'flex flex-col gap-5'}>
           <CollaborationCard />
           <CollaborationCard />
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,22 @@
 import {createBrowserRouter} from "react-router-dom";
 import Home from "./pages/Home";
 import Collaborations from "./pages/Collaborations";
+import Collaboration from "./pages/Collaboration";
 
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home/>
-	},
-  {path: "/collabs", element: <Collaborations />}
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/collabs",
+    element: <Collaborations/>
+  },
+  {
+    path: '/collabs/123',
+    element: <Collaboration />
+  }
 ])
 
 export default router
