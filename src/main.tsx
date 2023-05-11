0b613fec5bdf7f4f595b0559f6678dc40a1c0805
jakhongir.tashpulatov <jakhongir.tashpulatov@publicisgroupe.cz>
2023-05-11 20:56:19 +0200

    firebase: init hosting

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,20 @@ import App from './App'
 import "./index.css"
 import {QueryClient} from "@tanstack/react-query";
 
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsfF-fRVH65gzjpxUZmizI3PjWFRB6RfA",
+  authDomain: "dappfluence.firebaseapp.com",
+  projectId: "dappfluence",
+  storageBucket: "dappfluence.appspot.com",
+  messagingSenderId: "378671163511",
+  appId: "1:378671163511:web:2f3c5b31d98c03be2cfe72"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
