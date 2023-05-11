9f35394306440e9d35465bbd91ca2b1c3934afcd
jakhongir.tashpulatov <jakhongir.tashpulatov@publicisgroupe.cz>
2023-05-11 20:41:17 +0200

    added company component

---
+++ src/pages/Company.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react'
+
+const Company: FC = () => {
+
+  return <div>
+    company
+  </div>
+}
+
+
+export default Company;
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,16 @@
 import {createBrowserRouter} from "react-router-dom";
 import Home from "./pages/Home";
 import Collaborations from "./pages/Collaborations";
+import Company from "./pages/Company";
 
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home/>
-	},
-  {path: "/collabs", element: <Collaborations />}
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {path: "/collabs", element: <Collaborations/>},
+  {path: "/company", element: <Company/>}
 ])
 
 export default router
