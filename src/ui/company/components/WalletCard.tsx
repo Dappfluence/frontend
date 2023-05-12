f6020a148c54258ead541527a74ecd073c6ca5e1
jakhongir.tashpulatov <jakhongir.tashpulatov@publicisgroupe.cz>
2023-05-12 14:28:00 +0200

    wallet card: fixed balance

--- src/ui/company/components/WalletCard.tsx
+++ src/ui/company/components/WalletCard.tsx
@@ -15,13 +15,13 @@ const WalletCard: FC<IProps> = ({children, className = ''}) => {
   const {address, isConnected} = useAccount()
   return (
     <Card className={'col-span-1'}>
-      {!isConnected && <div className="mx-auto">
-        <ConnectButton/>
-      </div>}
-      <div className={'flex flex-col'}>
-        <span>Connected wallet</span>
-        <span className={'text-xl font-bold'}>{address?.slice(0, 5)}...{address?.slice(36)}</span>
-      </div>
+      {!isConnected ? <div className="mx-auto">
+          <ConnectButton/>
+        </div> :
+        <div className={'flex flex-col'}>
+          <span>Connected wallet</span>
+          <span className={'text-xl font-bold'}>{address?.slice(0, 5)}...{address?.slice(36)}</span>
+        </div>}
     </Card>
   );
 };
